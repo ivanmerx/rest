e4663eeed61976ae9498b8913bdd0785a1db54f7
Ivan Merxbauer <ivan.merxbauer@seznam.cz>
2017-10-30 20:10:58 +0100

    Init

---
+++ REST-master/REST/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using REST.Entity;
+using REST.WebClient;
+namespace REST
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void GetPersonListAndPopulateAsync()
+        {
+            App.Database.ResetTable();
+            Console.WriteLine("Is current thread in background: " + Thread.CurrentThread.IsBackground);
+
+            Rest webClient = new Rest();
+            List<Photo> data = await webClient.GetPersonsListAsync(this);
+            if (data != null && data.Count != 0)
+            {
+                if (!String.IsNullOrEmpty(selectid.Text))
+                {
+                    ObservableCollection<Photo> pl = new ObservableCollection<Photo>(data);
+                    await App.Database.SaveItemsAsync(data);
+                    ListBox.ItemsSource = App.Database.GetItemsNotDoneAsync2(Int32.Parse(selectid.Text)).Result;
+                }
+                else
+                {
+                    ObservableCollection<Photo> pl = new ObservableCollection<Photo>(data);
+                    await App.Database.SaveItemsAsync(data);
+                    ListBox.ItemsSource = App.Database.GetItemsNotDoneAsync().Result;
+                }
+            }
+        }
+
+        private async void GetData_Click2(object sender, RoutedEventArgs e)
+        {
+            GetPersonListAndPopulateAsync();
+        }
+    }
+}
---
+++ REST-master/REST/WebClient/Rest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using REST.Entity;
+using REST.Interfaces;
+
+namespace REST.WebClient
+{
+    class Rest
+    {
+        public async Task<List<Photo>> GetPersonsListAsync(MainWindow main)
+        {
+            string url = "http://jsonplaceholder.typicode.com";
+            var client = new RestClient(url);
+
+            var request = new RestRequest("/photos", Method.GET);
+
+            IRestResponse response = client.Execute(request);
+
+            IParser parser = new JsonParser();
+            return await parser.ParseStringAsync<List<Photo>>(response.Content);
+
+        }
+    }
+}
